0db72582e243e5f95d0e9a1bd4a28a44729fde63
MatecGM <mateo.bico09@gmail.com>
2024-01-04 18:38:36 +0100

    fjciqof

--- Makefile
+++ Makefile
@@ -2,25 +2,31 @@ CC = cc
 
 #FLAGS = -Wall -Wextra -Werror
 
-NAME = so_long
+NAME = fdf
 
 SRCS = test.c\
 
 OBJS = $(SRCS:.c=.o)
+MLX = MacroLibX/libmlx.so
 
 all : $(NAME)
 
-$(NAME): $(OBJS)
-	$(CC) $(OBJS) -o $(NAME) -LMacroLibX/limlx.so -lmlx -lXext -lX11
+$(NAME): $(OBJS) $(MLX)
+	$(CC) $(OBJS) -o $(NAME) -lXext -lX11 $(MLX) -lSDL2 -g
+
+$(MLX):
+	make -C MacroLibX -j
 
 .c.o:
 	$(CC) $(FLAGS) -I/usr/include -I./MacroLibX/includes -O3 -c $< -o $@
 
 clean:
 	rm -f $(OBJS)
+	make -C MacroLibX clean
 
 fclean: clean
 	rm -f $(NAME)
+#make -C MacroLibX fclean
 
 re: fclean all
 
--- test.c
+++ test.c
@@ -6,30 +6,40 @@
 /*   By: mbico <mbico@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 15:21:44 by mbico             #+#    #+#             */
-/*   Updated: 2024/01/02 16:21:31 by mbico            ###   ########.fr       */
+/*   Updated: 2024/01/04 18:26:50 by mbico            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MacroLibX/includes/mlx.h"
 #include <stdio.h>
 #include "FDF.h"
+#include <stdlib.h>
 
-int	close(int keycode, t_mlx *mlx)
+int	ft_close(int keycode, void *vars)
 {
-	(void) keycode;
-	mlx_destroy_window(mlx->mlx, mlx->win);
+	t_mlx *v;
+
+	v = (t_mlx *) vars;
+	printf("%d\n", keycode);
+	if (keycode == 41)
+	{
+		mlx_destroy_window(v->mlx, v->win);
+		mlx_destroy_display(v->mlx);
+		exit(EXIT_SUCCESS);
+	}
 	return (0);
 }
 
 int	main(void)
 {
 	void	*img;
-	t_mlx	mlx;
+	t_mlx	*vars;
 	int		img_width;
 	int		img_height;
 
-	mlx.mlx = mlx_init();
-	mlx.win = mlx_new_window(mlx.mlx, 1024, 600, "So long");
-	mlx_on_event(mlx.mlx, mlx.win);
-	mlx_loop(mlx.mlx);
+	vars = malloc(sizeof(t_mlx));
+	vars->mlx = mlx_init();
+	vars->win = mlx_new_window(vars->mlx, 1024, 600, "FdF");
+	mlx_on_event(vars->mlx, vars->win, 0, ft_close, vars);
+	mlx_loop(vars->mlx);
 }
